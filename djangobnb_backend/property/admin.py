8f412477ffa79c38789345377fde365e142f5c88
GabrielGL <gabrielunagarcia@hotmail.com>
2025-03-19 20:58:42 -0600

    11 book property

--- djangobnb_backend/property/admin.py
+++ djangobnb_backend/property/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Property
+from .models import Reservation
 
 admin.site.register(Property)
+admin.site.register(Reservation)
+
--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -7,7 +7,7 @@ from rest_framework.authentication import SessionAuthentication
 
 
 from .forms import PropertyForm
-from .models import Property
+from .models import Property, Reservation
 from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
 
 @api_view(['GET'])
@@ -51,3 +51,29 @@ def create_property(request):
 
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
+
+@api_view(['POST'])
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property = property,
+            start_date = start_date,
+            end_date = end_date,
+            number_of_nights = number_of_nights,
+            total_price = total_price,
+            guests = guests,
+            created_by = request.user
+        )
+        return JsonResponse({'success':True})
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'succes':False})
